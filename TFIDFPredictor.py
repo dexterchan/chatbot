cd8bd2b3dae6fc8e3f21397f180fc7dac361dde0
dexterchan <dexterchan@gmail.com>
2017-05-03 11:32:41 +0800

    firstry

---
+++ TFIDFPredictor.py
@@ -0,0 +1,18 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+class TFIDFPredictor:
+    def __init__(self):
+        self.vectorizer = TfidfVectorizer()
+ 
+    def train(self, data):
+        self.vectorizer.fit(np.append(data.Context.values,data.Utterance.values))
+ 
+    def predict(self, context, utterances):
+        # Convert context and utterances into tfidf vector
+        vector_context = self.vectorizer.transform([context])
+        vector_doc = self.vectorizer.transform(utterances)
+        # The dot product measures the similarity of the resulting vectors
+        result = np.dot(vector_doc, vector_context.T).todense()
+        result = np.asarray(result).flatten()
+        # Sort by top results and return the indices in descending order
+        return np.argsort(result, axis=0)[::-1]
